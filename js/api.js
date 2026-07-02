507093bfc265a162b27bf74746afaaf075189638
kenta-urus <housei.kouguchi@himeno.or.jp>
2026-07-02 11:01:52 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://scan-cdt-versus-weight.trycloudflare.com";
+const baseUrl = "https://law-humanity-hunter-farm.trycloudflare.com";
 
 async function fetchPatientInfo(userId) {
     const url = `${baseUrl}/patient?line_user_id=${userId}`;
