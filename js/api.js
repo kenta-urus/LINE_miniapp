cb51acee4a4276deedc4aa37041ee262a27a9706
kenta-urus <housei.kouguchi@himeno.or.jp>
2026-07-07 09:09:54 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 //  cloudflared URL
-const baseUrl = "https://amend-sequence-chapter-exhibits.trycloudflare.com/test";
+const baseUrl = "https://amend-sequence-chapter-exhibits.trycloudflare.com";
 
 async function fetchPatientInfo(userId) {
     const url = `${baseUrl}/patient?line_user_id=${userId}`;
