19ca928760ea21b367b43932675053eb6a6fe9e8
kenta-urus <housei.kouguchi@himeno.or.jp>
2026-07-17 16:52:41 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-//export const baseUrl = "https://xxxxx.trycloudflare.com";
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = "https://yacht-catherine-outlets-interior.trycloudflare.com";
+//export const baseUrl = "http://localhost:8000";
